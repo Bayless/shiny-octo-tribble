988fcf3a6fa927ca7a51dba904a83fae1981d86e
Bayless <bsmithsalzberg@stuy.edu>
2017-04-28 07:12:01 -0400

    yay btw dont stack

--- decorating.py
+++ decorating.py
@@ -2,29 +2,38 @@
 import time
 
 def timer(f):
-    def inner(num):
+    def inner(*num):
         first = time.time()
-        f(num)
+        f(*num)
         return time.time() - first
     return inner
 
 def name(f):
-    def inner(num):
+    def inner2(*num):
         name = f.func_name
-        variables = str(num)
-        return "name: " + name + "\nparameters: " + variables + "\n"
-    return inner + str(f.func_name)
+        variables = ""
+        for i in num:
+            variables+=str(i)+", "
+        return "name: " + name + "\nparameters: " + variables[:len(variables)-2] + "\n"
+    return inner2
 
 @name
-@timer
+#@timer
 def fib(i):
     if i == 1 or i == 2:
         return 1
     return fib(i-1)+fib(i-2)
-    
+
+@name
+#@timer
+def adding(q,w,e,r,t):
+    return q+w+e+r+t
+
+
 for i in range(1,36):
     print str(i) + ": "+ str(fib(i))
 
+print adding(1,2,3,4,5)
 #closure = wrapper(fib)
 #closure(3) #will run foo with the arguments -2, 3, 'hello' through wrapper
 #time.time()
